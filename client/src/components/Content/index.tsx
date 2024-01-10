791752f35143b88c78f864e179824bd5067f0dc7
Hajar Amakhzoun <hajar.amakhzoun@etu.univ-lyon1.fr>
2024-01-10 19:53:56 +0100

    deleting style

--- client/src/components/Content/index.tsx
+++ client/src/components/Content/index.tsx
@@ -2,66 +2,16 @@ import * as React from "react";
 import img1 from "../../images/img1.png";
 import img2 from "../../images/img2.png";
 import img3 from "../../images/img3.png";
-import "tw-elements-react/dist/css/tw-elements-react.min.css";
-import { TECarousel, TECarouselItem } from "tw-elements-react";
 
 
 export  function Content()  {
   return (
-    <>
-      <TECarousel showControls showIndicators ride="carousel">
-        <div className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
-          <TECarouselItem
-            itemID={1}
-            className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-          >
-            <img
-              src={img1}
-              className="block w-full"
-              alt="..."
-            />
-            <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
-              <h5 className="text-xl">First slide label</h5>
-              <p>
-                Some representative placeholder content for the first slide.
-              </p>
-            </div>
-          </TECarouselItem>
-          <TECarouselItem
-            itemID={2}
-            className="relative float-left hidden -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-          >
-            <img
-              src={img2}
-              className="block w-full"
-              alt="..."
-            />
-            <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
-              <h5 className="text-xl">Second slide label</h5>
-              <p>
-                Some representative placeholder content for the second slide.
-              </p>
-            </div>
-          </TECarouselItem>
-          <TECarouselItem
-            itemID={3}
-            className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-          >
-            <img
-              src={img3}
-              className="block w-full"
-              alt="..."
-            />
-            <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
-              <h5 className="text-xl">Third slide label</h5>
-              <p>
-                Some representative placeholder content for the third slide.
-              </p>
-            </div>
-          </TECarouselItem>
-        </div>
-      </TECarousel>
-    </>);
+   <div>
+<img src={img1} alt="img1" />
+<img src={img2} alt="img2" />
+<img src={img3} alt="img3" />
+   </div>
+    );
 }
 
 
