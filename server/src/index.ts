54a280eb8e328b2a574a7925fe9c82aec8468f7e
Hajar Amakhzoun <hajar.amakhzoun@etu.univ-lyon1.fr>
2024-01-10 16:40:20 +0100

    modifs

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,14 @@ import { HelloRouteur } from './routes/hello.router';
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+const path = require("path");
+const DIST_DIR = path.join(__dirname, "../../client/dist");
+const HTML_FILE = path.join(DIST_DIR, "index.html");
 app.listen(port, () => {
   process.stdout.write(`Server started on port: ${port}\n`);
 });
 
 app.use('/hello', HelloRouteur);
+
+app.use(express.static(DIST_DIR));
+app.use('/',express.static(HTML_FILE));
