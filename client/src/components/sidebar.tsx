993f766131d9566162b1013f5f3949a8f3dceb7c
AMAKHZOUN HAJAR p2312644 <hajar.amakhzoun@etu.univ-lyon1.fr>
2024-03-03 20:19:16 +0000

    Update sidebar.tsx

--- client/src/components/sidebar.tsx
+++ client/src/components/sidebar.tsx
@@ -120,7 +120,7 @@ const sidebar = () => {
 
           <div className="modal-action flex justify-end">
             <form method="dialog">
-              <button type="submit" className="text-slate-100 bg-slate-300 hover:bg-slate-500  focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800 lg:mr-11 sm:mr-3 " >Cancel</button>
+              <button type="submit" className="text-slate-100 bg-slate-300 hover:bg-slate-500  focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800 lg:mr-5 sm:mr-3 " >Cancel</button>
               <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-7 py-2.5 mr-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 " onClick={add}>Add</button>
             </form>
           </div>
